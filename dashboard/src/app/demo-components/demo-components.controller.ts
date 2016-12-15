36ef521f3e1d6d0df9a94b8cf3a5bd3d155dbf55
Oleksii Kurinnyi <okurinnyi@codenvy.com>
2016-12-15 11:27:55 +0200

    improve demoComponents component
    Signed-off-by: Oleksii Kurinnyi <okurinnyi@codenvy.com>

--- dashboard/src/app/demo-components/demo-components.controller.ts
+++ dashboard/src/app/demo-components/demo-components.controller.ts
@@ -10,23 +10,56 @@
  */
 'use strict';
 
+enum Tab {
+  FONT,
+  PANEL,
+  SELECTER,
+  ICONS,
+  BUTTONS,
+  INPUT,
+  LIST
+}
+
 /**
  * This class is handling the controller for the demo of components
  * @author Florent Benoit
  */
-export class DemoComponentsCtrl {
+export class DemoComponentsController {
+  $location: ng.ILocationService;
+  $routeParams: ng.route.IRouteParamsService;
+
+  selectedTabIndex: number = 0;
+  tab: Object = Tab;
+
+  booksByAuthor: {
+    [author: string]: any[];
+  };
+
+  button2Disabled: boolean;
+
+  listItemsDocs: string[];
+
+  listItemsTasks: any[];
 
   /**
    * Default constructor that is using resource
    * @ngInject for Dependency injection
    */
-  constructor() {
+  constructor($location: ng.ILocationService, $routeParams: ng.route.IRouteParamsService) {
+    this.$location = $location;
 
+    let requestedDemoTab = ($routeParams as any).demo || Tab[Tab.FONT];
+    this.selectedTabIndex = Tab[requestedDemoTab.toUpperCase()] || 0;
+
+    this.init();
+  }
+
+  init() {
     // selecter
     this.booksByAuthor = {};
-    this.booksByAuthor['St Exupery'] = [{title:'The little prince'}];
-    this.booksByAuthor['V. Hugo'] = [{title:'Les miserables'}, {title:'The Hunchback of Notre-Dame'}];
-    this.booksByAuthor['A. Dumas'] = [{title:'The count of Monte Cristo'}, {title:'The Three Musketeers'}];
+    this.booksByAuthor['St Exupery'] = [{title: 'The little prince'}];
+    this.booksByAuthor['V. Hugo'] = [{title: 'Les miserables'}, {title: 'The Hunchback of Notre-Dame'}];
+    this.booksByAuthor['A. Dumas'] = [{title: 'The count of Monte Cristo'}, {title: 'The Three Musketeers'}];
 
 
     this.button2Disabled = true;
@@ -37,6 +70,17 @@ export class DemoComponentsCtrl {
       {name : 'Task 4', done: true}, {name : 'Task 5', done: false}];
   }
 
+  /**
+   * Use current tab as query parameter for this tab to be selected after demoComponents is reloaded.
+   *
+   * @param tabIndex {number} current tab index
+   */
+  setDemoTab(tabIndex: number): void {
+    // remove any query from url
+    this.$location.url(this.$location.path());
+
+    this.$location.search('demo', Tab[tabIndex]);
+  }
 
   toggleDisabled2() {
     this.button2Disabled = !this.button2Disabled;
